4956648cc80924ab1df36b8dce130d639047e80c
Igor Sakhno <ihar.sakhno@gmail.com>
2020-01-16 11:35:29 +0300

    Find word in list by first letter(fixed)

--- List begining A.py
+++ List begining A.py
@@ -1,4 +1,5 @@
-begin = input()
+letter = input("Pelse enter the first letter: ")
+begin = input("Please enter the list of words: ")
 begin_2 = begin.split()
-final = [x for x in begin_2 if x.lower().startswith('a')]
+final = [x for x in begin_2 if x.lower().startswith(letter)]
 print(final)
